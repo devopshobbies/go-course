07d4d4d2f02f1abae5087f5c6aacebcf807869ff
mohammadne <mohammadne.dev@gmail.com>
2023-04-28 15:01:41 +0330

    feat: update worker pool

--- worker-pool/main.go
+++ worker-pool/main.go
@@ -1,54 +1,44 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net/http"
+	"os"
+	"sync"
 	"time"
 )
-
-var websites = []string{
-	"https://google.com/",
-	"https://github.com/",
-	"https://www.linkedin.com/",
-	"http://medium.com/",
-	"https://golang.org/",
-	"https://www.udemy.com/",
-	"https://www.coursera.org/",
-	"https://wesionary.team/",
-}
 
 func main() {
 	startTime := time.Now()
 	jobs := make(chan string, 5)
-	counter := make(chan struct{}, len(websites))
+	var wg sync.WaitGroup
 
 	const workers = 3
 	for worker := 0; worker < workers; worker++ {
-		go getWebsite(jobs, counter, worker)
+		go getWebsite(jobs, &wg, worker)
 	}
 
-	// populate jobs
-	for index := 0; index < len(websites); index++ {
-		jobs <- websites[index]
+	file := "../websites.txt"
+	reader, _ := os.Open(file)
+	scanner := bufio.NewScanner(reader)
+	for scanner.Scan() {
+		jobs <- scanner.Text()
+		wg.Add(1)
 	}
 
-	// sleep until all the jobs are finished
-	// can also be implemented by wait-groups
-	for index := 0; index < len(websites); index++ {
-		<-counter
-	}
-
+	wg.Wait()
 	fmt.Println("totall time: ", time.Since(startTime))
 }
 
 // workers
-func getWebsite(websites chan string, counter chan struct{}, worker int) {
+func getWebsite(websites chan string, wg *sync.WaitGroup, worker int) {
 	for website := range websites {
 		if res, err := http.Get(website); err != nil {
 			fmt.Printf("%s is down, proccessed by worker %d", website, worker)
 		} else {
 			fmt.Printf("[%d] %s is up, proccessed by worker %d\n", res.StatusCode, website, worker)
 		}
-		counter <- struct{}{}
+		wg.Done()
 	}
 }
